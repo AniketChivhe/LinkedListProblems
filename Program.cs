4f20ced8f849724ada5f547ae80c92f5bafb79c6
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-11 23:27:23 +0530

    [Aniket]UC 1 added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Linked List Program");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            
+            list.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
