ecc8c9df3a9aad88f6f5ef91d54fcc0be973dc49
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:35:55 +0530

    [Aniket]UC6-added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6_DeleteLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Linked List Program UC3");
+            LinkedListUC6 list = new LinkedListUC6();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.RemovaLastNode();
+            Console.WriteLine("Last element deleted");
+            list.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
+
