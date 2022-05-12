6efe0f17ef6673acb96442f9b49629e1bfcea9bf
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:32:01 +0530

    [Aniket]UC4-added

---
+++ LinkedListUC4.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_InsertInBetween
+{
+    internal class LinkedListUC4
+    {
+        internal Node head; //new 
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position != 0) //
+                {
+
+                    if (position >= 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine( "{0} Number added in Between ", data);
+            }
+            return head;
+        }
+
+
+
+
+        internal void Display()
+        {
+            Console.WriteLine("***Displaying Nodes***");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_InsertInBetween
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Linked List Program UC3");
+            LinkedListUC4 list = new LinkedListUC4();
+            list.Add(56);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 30);
+
+            list.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
