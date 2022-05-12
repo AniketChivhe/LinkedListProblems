fd1cd15740166760e5fd1b153c17742505e24c8e
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:39:00 +0530

    [Aniket]UC8-added

---
+++ LinkedListUC8.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UC8_SearchAndInsert
+{
+    internal class LinkedListUC8
+    {
+        //Creating a head node to point the first element
+        public Node head;
+        //Creating a method to insert last 
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        
+        
+        //Searching the particular element in linked list
+        public Node Search(int value)
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                if (temp.data == value)
+                {
+                    Console.WriteLine("Element found.. " + value);
+                    return temp;
+                }
+                temp = temp.next;
+
+            }
+            Console.WriteLine("Element not found!!!!!!!!");
+            return default;
+        }
+        //Searching the particular element
+        //-->If element is found,then after that insert the element..
+        public int Insert(int searchdata, int data)
+        {
+            Node temp = Search(searchdata);
+            Node newNode = new Node(data);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            return (newNode.data);
+
+        }
+        
+        
+        
+
+
+        //Display the nodes
+        public void Display()
+        {
+            Console.WriteLine("**********Displaying Nodes************");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC8_SearchAndInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Linked List Program UC8");
+            LinkedListUC8 list = new LinkedListUC8();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            Console.WriteLine("\n Enter any of 56,30,70 to Find in Linked list and add 40 to next : ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 56:
+                    list.Search(56);
+                    list.Insert(56, 40);
+                    break;
+                case 30:
+                    list.Search(30);
+                    list.Insert(30, 40);
+                    break;
+                case 70:
+                    list.Search(70);
+                    list.Insert(70, 40);
+                    break;
+            }
+            list.Display();
+            Console.ReadKey();
+        }
+    }
+}
+
+
