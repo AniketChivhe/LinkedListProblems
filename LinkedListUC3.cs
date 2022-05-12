d90514018e9ae17c1a2e51fe8fee22f12806966b
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-12 16:49:08 +0530

    [Aniket]UC3 added

---
+++ LinkedListUC3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_Appending
+{
+    internal class LinkedListUC3
+    {
+        internal Node head; //new 
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        public void Append(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.data = new_data;//assign data element
+            new_node.next = null;//assign null to next of new node
+                                 //check linkedlist empty 
+            if (head == null)
+            {
+                head = new_node;
+
+            }
+            else
+            {
+                //traverse to last node
+                Node temp = new Node(new_data);
+                temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+
+            }
+        }
+
+
+
+        internal void Display()
+        {
+            Console.WriteLine("***Displaying Nodes***");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+
+        }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_Appending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Linked List Program UC3");
+            LinkedListUC3 list = new LinkedListUC3();
+            list.Add(56);
+            list.Append(30);
+            list.Append(70);
+
+            list.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
