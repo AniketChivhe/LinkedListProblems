4d9a510555175f431fead33344622dfb2303269d
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:40:09 +0530

    [Aniket]UC9-added

---
+++ LinnkedListUC9.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC9_SearchAndDelete
+{
+    internal class LinnkedListUC9
+    {
+        //Creating a head node to point the first element
+        public Node head;
+        //Creating a method to insert 
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        //Searching the particular element in linked list
+        public Node Search(int value)
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                if (temp.data == value)
+                {
+                    Console.WriteLine("Element found.. " + value);
+                    return temp;
+                }
+                temp = temp.next;
+
+            }
+            Console.WriteLine("Element not found!!!!!!!!");
+            return default;
+        }
+        
+
+
+
+        //search and delete
+
+        public int DeleteInBetween(int data)
+        {
+            Node temp = Search(data);
+            Console.WriteLine("Data {0} has been deleted from linked list", temp.data);
+            temp.next = temp.next.next;
+            int sizeofList = Size();
+            return sizeofList;
+        }
+
+        public int Size()
+        {
+            int count = 0;
+            Node temp = this.head;
+            while (temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+   
+     
+
+
+        //Display the nodes
+        public void Display()
+        {
+            Console.WriteLine("**********Displaying Nodes************");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC9_SearchAndDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            LinnkedListUC9 list = new LinnkedListUC9();
+            list.Add(56);
+            list.Add(30);
+            list.Add(40);
+            list.Add(70);
+            list.Search(40);
+            list.DeleteInBetween(40);
+            
+
+            list.Display();
+            Console.ReadLine();
+        }
+                  
+    }
+}
