119769c4f6f322e329f43bc58c2c56468a62c222
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:42:01 +0530

    [Aniket]UC10-added

---
+++ LinkedListUC10.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC10_SortingLinkedlist
+{
+    internal class LinkedListUC10
+    {
+        //Creating a head node to point the first element
+        public Node head;
+        //Creating a method to insert 
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        public void Sort()
+        {
+            Node i, j;
+            int temp;
+            for (i = this.head; i.next != null; i = i.next)
+            {
+                for (j = i.next; j != null; j = j.next)
+                {
+                    if (i.data > j.data)
+                    {
+                        temp = i.data;
+                        i.data = j.data;
+                        j.data = temp;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+        //Display the nodes
+        public void Display()
+        {
+            Console.WriteLine("**********Displaying Nodes************");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC10_SortingLinkedlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedListUC10 list = new LinkedListUC10();
+            list.Add(56);
+            list.Add(30);
+            list.Add(40);
+            list.Add(70);
+            Console.WriteLine("Sorting Of Linked List");
+            list.Sort();
+
+            list.Display();
+            Console.ReadLine();
+        }
+
+    }
+}
