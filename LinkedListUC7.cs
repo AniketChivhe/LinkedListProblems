ec907aa283b591b0e14cadfe9c7491282898c6d8
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-13 03:37:08 +0530

    [Aniket]UC7-added

---
+++ LinkedListUC7.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC7_Search
+{
+    internal class LinkedListUC7
+    {
+        internal Node head; //new 
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        internal Node Search(int value)
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                if (temp.data == value)
+                {
+                    Console.WriteLine("Element Serached  :" + value);
+                    return this.head;
+                }
+                temp = temp.next;
+            }
+            Console.WriteLine("Element not found!!!!!!!!");
+            return default;
+        }
+
+
+        internal void Display()
+        {
+            Console.WriteLine("***Displaying Nodes***");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC7_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To Linked List Program UC3");
+            LinkedListUC7 list = new LinkedListUC7();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+            Console.WriteLine("Enter any of 56,30,70 to Find in Linked list: ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 56:
+                    list.Search(56);
+                    break;
+                case 30:
+                    list.Search(30);
+                    break;
+                case 70:
+                    list.Search(70);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
